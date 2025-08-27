cf5851c560ad867fa9a1032b852e7ffe735d01f0
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2025-08-27 15:23:23 +0200

    update

--- docs/examples/classifier_wavenet.py
+++ docs/examples/classifier_wavenet.py
@@ -19,9 +19,9 @@ We will:
 # Imports and reproducibility
 # --------------------------
 import numpy as np
-import matplotlib.pyplot as plt
-from DeepPeak.signals import SignalDatasetGenerator, Kernel
+
 from DeepPeak.machine_learning.classifier import WaveNet
+from DeepPeak.signals import Kernel, SignalDatasetGenerator
 
 np.random.seed(42)
 
@@ -31,10 +31,7 @@ np.random.seed(42)
 NUM_PEAKS = 3
 SEQUENCE_LENGTH = 200
 
-generator = SignalDatasetGenerator(
-    n_samples=100,
-    sequence_length=SEQUENCE_LENGTH
-)
+generator = SignalDatasetGenerator(n_samples=100, sequence_length=SEQUENCE_LENGTH)
 
 dataset = generator.generate(
     signal_type=Kernel.GAUSSIAN,
@@ -44,7 +41,7 @@ dataset = generator.generate(
     width=(0.03, 0.05),
     noise_std=0.1,
     categorical_peak_count=False,
-    compute_region_of_interest=True
+    compute_region_of_interest=True,
 )
 
 # %%
@@ -60,9 +57,9 @@ dense_net = WaveNet(
     num_filters=64,
     num_dilation_layers=6,
     kernel_size=3,
-    optimizer='adam',
-    loss='binary_crossentropy',
-    metrics=['accuracy']
+    optimizer="adam",
+    loss="binary_crossentropy",
+    metrics=["accuracy"],
 )
 dense_net.build()
 dense_net.summary()
@@ -75,18 +72,15 @@ history = dense_net.fit(
     dataset.region_of_interest,
     validation_split=0.2,
     epochs=20,
-    batch_size=64
+    batch_size=64,
 )
 
 # %%
 # Plot training history
 # ---------------------
-dense_net.plot_model_history(history)
+dense_net.plot_model_history()
 
 # %%
 # Predict and visualize on a test signal
 # --------------------------------------
-dense_net.plot_prediction(
-    signal=dataset.signals[0:1, :],
-    threshold=0.4
-)
+dense_net.plot_prediction(signal=dataset.signals[0:1, :], threshold=0.4)
